ff2a3f35550f0f4c94d0473237a5f5afcee24b66
Nathan Maciel <nathannmvr@gmail.com>
2023-06-27 21:54:26 -0300

    Testagem

--- test/test01.spec.js
+++ test/test01.spec.js
@@ -1,2 +1,24 @@
-import { test } from "vitest";
+import { beforeAll, expect, test } from "vitest";
+import request from "supertest";
+import { app } from "../src/app.js";
 
+beforeAll(async () => {
+    await app.ready();
+})
+
+test('Testar o código de retorno do post', async () => {
+    await request(app.server)
+        .post('/users')
+        .send({
+            name: 'teste',
+            email: 'email@teste.com'
+        }).expect(201);
+    //const codigo = 500;
+    //expect(codigo).toEqual(200);
+})
+
+test('Testar o código de retorno do get', async () => {
+    await request(app.server)
+        .get('/users')
+        .expect(200);
+})
